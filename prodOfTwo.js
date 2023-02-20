2a921bd2d4680af20609e145c6bdc2a36b108bf4
Krisha8179 <krishnarao1440@gmail.com>
2023-02-20 22:20:39 +0530

    prduct & creating obj

---
+++ prodOfTwo.js
@@ -0,0 +1,14 @@
+// const prodOfTwoNumbers = (a,b) => a*b;
+
+// console.log(prodOfTwoNumbers(2,3));
+
+
+const student = {
+    Name: 'brendon',
+    age: 20,
+    greet() {
+        console.log(`Hi i am ${this.Name}`)
+    }
+}
+
+student.greet();
