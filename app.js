30dbb132ea8015471149fbe3d4c97e41858ccb9e
Krisha8179 <krishnarao1440@gmail.com>
2023-02-27 12:51:50 +0530

    app file

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-const { urlencoded } = require('body-parser');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name ="title"><input type="text" name ="size"><button type="submit">Add product</button></form>');
-});
-
-app.use('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
-});
-
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hello Express</h1>');
+app.use((req, res, next) =>{
+    res.status(404).send('<h1>Page not found</h1>');
 });
     
 
